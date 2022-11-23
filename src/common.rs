d5b6850d0b3d4295741da7233a7a95dc9e8d8a8e
Jakub Cendrowski <66736611+Quuba@users.noreply.github.com>
2022-11-23 20:46:34 +0100

    code refactoring

--- src/common.rs
+++ src/common.rs
@@ -1,22 +1,49 @@
 use serde_derive::{Deserialize, Serialize};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct NetworkMessage {
     pub text: String,
+    pub message_type: MessageType,
 }
 
 impl NetworkMessage {
-    pub fn new(message: String) -> Self {
-        Self { text: message }
+    pub fn new(message: String, message_type: MessageType) -> Self {
+        Self {
+            text: message,
+            message_type: message_type,
+        }
     }
 }
 
-//TODO: copy from server.rs / client.rs
-pub fn send_message(){
-
+#[derive(Serialize, Deserialize, Debug)]
+pub enum MessageType {
+    Other,
 }
 
-//TODO: copy from server.rs / client.rs
-pub fn wait_for_message(){
+pub async fn send_message(message: &NetworkMessage, stream: &mut TcpStream) -> Result<(), ()> {
+    let json: String = serde_json::to_string(message).unwrap();
+    match stream.write(json.as_bytes()).await {
+        Ok(_) => {
+            return Ok(());
+        }
+        Err(_) => {
+            return Err(());
+        }
+    }
+}
 
+pub async fn wait_for_message(stream: &mut TcpStream, buffer: &mut [u8]) -> NetworkMessage {
+    let n = stream
+        .read(buffer)
+        .await
+        .expect("failed to read data from socket");
+    let json = std::str::from_utf8(buffer).unwrap().replace('\0', "");
+    let message = serde_json::from_str::<NetworkMessage>(json.as_str())
+        .expect("error while trying to parse incoming data");
+    println!("received {:?}", message.text);
+    return message;
 }
--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,6 @@
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-};
+use tokio::net::TcpListener;
 
-use crate::common::NetworkMessage;
+use crate::common::{send_message, wait_for_message, MessageType, NetworkMessage};
 
 pub async fn run(ip: &str, port: &str) {
     let listener = TcpListener::bind(format!("{}:{}", ip, port)).await.unwrap();
@@ -14,61 +11,24 @@ pub async fn run(ip: &str, port: &str) {
         println!("new connection from {addr}");
         tokio::spawn(async move {
             let mut received_buffer = vec![0; 1024];
-            // let mut line = String::with_capacity(512);
-            // let mut received_count = 0;
 
+            let mut last_message: NetworkMessage =
+                NetworkMessage::new("".to_string(), MessageType::Other);
             // In a loop, read data from the socket and write the data back.
-            let mut last_message: NetworkMessage = NetworkMessage::new("".to_string());
             loop {
-                // let message = NetworkMessage::new("hello there".to_string());
-                // send_message(&message, &mut stream).await;
-                let new_received = wait_for_message(&mut received_buffer, &mut stream).await;
+                let new_received = wait_for_message(&mut stream, &mut received_buffer).await;
                 if new_received.text != last_message.text {
                     if new_received.text == "hello there".to_string() {
                         send_message(
-                            &NetworkMessage::new("general kenobi".to_string()),
+                            &NetworkMessage::new("general kenobi".to_string(), MessageType::Other),
                             &mut stream,
                         )
-                        .await;
+                        .await
+                        .unwrap();
                     }
                     last_message = new_received;
                 }
-
-                // let n = stream
-                //     .read(&mut buf)
-                //     .await
-                //     .expect("failed to read data from socket");
-
-                // println!("received: {:#}", std::str::from_utf8(&buf).unwrap());
-                // if n == 0 {
-                //     return;
-                // }
-                // received_count += 1;
-
-                // if received_count == 1 {
-                //     stream
-                //         .write("ganske pikk".as_bytes())
-                //         .await
-                //         .expect("failed to write data to socket");
-                // }
             }
         });
     }
-}
-
-async fn send_message(message: &NetworkMessage, stream: &mut TcpStream) {
-    println!("sent a message");
-    let json: String = serde_json::to_string(message).unwrap();
-    stream.write(json.as_bytes()).await.unwrap();
-}
-
-async fn wait_for_message(buffer: &mut [u8], stream: &mut TcpStream) -> NetworkMessage {
-    let n = stream
-        .read(buffer)
-        .await
-        .expect("failed to read data from socket");
-    let json = std::str::from_utf8(buffer).unwrap().replace('\0', "");
-    let message = serde_json::from_str::<NetworkMessage>(json.as_str()).unwrap();
-    println!("received {:?}", message.text);
-    return message;
 }
